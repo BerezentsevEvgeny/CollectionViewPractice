c65342c21ae3bfd075e693cf916453b1c50e91a6
Berezentsev Evgeny <jonn09@icloud.com>
2021-05-23 12:18:19 +0300

    Initial Commit

---
+++ BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Евгений Березенцев on 29.04.2021.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var label: UILabel!
+    
+}
